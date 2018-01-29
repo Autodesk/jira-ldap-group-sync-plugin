d93be4a8d22c42e92d560687e8e8a9edb34fdccd
prasadveautodesk <venkat.prasad@autodesk.com>
2018-01-29 18:13:32 +0800

    updated pomadded paging code

--- src/main/resources/js/jira-ldap-group-sync-plugin.js
+++ src/main/resources/js/jira-ldap-group-sync-plugin.js
@@ -1,6 +1,8 @@
-function adskldapsyncconfig_filter(element) {
+/* global AJS */
+
+function adskldapsyncconfig_filter() {
   // Declare variables 
-  var count, input, filter, table, tr, td, i;
+  var count, input, filter, table, tr, i;
   count = document.getElementById("adskldapsyncconfig_count");
   input = document.getElementById("adskldapsyncconfig_search");
   filter = input.value.toUpperCase();
@@ -10,17 +12,17 @@ function adskldapsyncconfig_filter(element) {
   // Loop through all table rows, and hide those who don't match the search query
   var size = 0;
   for (i = 0; i < tr.length; i++) {
-    td0 = tr[i].getElementsByTagName("td")[1];
-    td1 = tr[i].getElementsByTagName("td")[2];
+    var td0 = tr[i].getElementsByTagName("td")[1];
+    var td1 = tr[i].getElementsByTagName("td")[2];
     if (td0 || td1) {
       if (td0.innerHTML.toUpperCase().indexOf(filter) > -1 || 
               td1.innerHTML.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
-        size++;
+        size++;        
       } else {
-        tr[i].style.display = "none";
+        tr[i].style.display = "none";        
       }
     }
   }
-  count.innerHTML = size;
+  count.innerHTML = size;  
 }
